73a8eb18007e6d1a5837291acb0a2ae58db1607b
wangke568 <tuplelist@163.com>
2020-09-25 23:22:01 +0800

    添加json快捷助手函数

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -22,3 +22,27 @@ if (! function_exists('include_route_files')) {
         }
     }
 }
+
+if (!function_exists('json')){
+    /**
+     * @param int $code code码
+     * @param string $message 提示
+     * @param null $data 数据
+     * @param int $statusCode 状态码
+     * @param array $headers
+     * @param int $options 常量  （JSON_UNESCAPED_SLASHES JSON_PRETTY_PRINT JSON_NUMERIC_CHECK）
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function json(int $code, string $message, $data=null,$statusCode=200,array $headers = [],$options=0){
+        $result = [
+            'code' => $code,
+            'message' => $message,
+        ];
+        if (!$data) {
+            $result['data'] = [];
+        }else{
+            $result['data'] = $data;
+        }
+        return response()->json($result,$statusCode,$headers,$options);
+    }
+}
